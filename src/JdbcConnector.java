2590289d185e11ed20b4f02b110ba9b9b40944c6
Vedant <147836209+Vedant363@users.noreply.github.com>
2024-02-05 21:32:13 +0530

    Updated

--- src/JdbcConnector.java
+++ src/JdbcConnector.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcConnector {
-    private static final String JDBC_URL = "jdbc:mysql://localhost/vedantdb"; // Replace with your MySQL host and database name
+    private static final String JDBC_URL = "jdbc:mysql://localhost/database"; // Replace with your MySQL host and database name
     private static final String USERNAME = "root"; // Replace with your MySQL username
-    private static final String PASSWORD = "ved363"; // Replace with your MySQL password
+    private static final String PASSWORD = ""; // Replace with your MySQL password
 
     private Connection connection;
 
